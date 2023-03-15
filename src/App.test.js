f99f51e248418d73b6f2a1148b4dd4bd1b6237fb
Alan <alanross2006@gmail.com>
2023-03-15 21:48:16 +0000

    fix test name

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
+test("renders SNAP! header", () => {
   render(<App />);
   const header = screen.getByText(/snap!/i);
   expect(header).toBeVisible();
